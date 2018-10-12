3115476af9b708cb18c903087e8873733ff4b303
JoMinJi <43244155+KKonzi@users.noreply.github.com>
2018-10-12 10:14:11 +0900

    Update gugudan.py

--- gugudan.py
+++ gugudan.py
@@ -1,6 +1,6 @@
 print("=========1~8단=========\n")
-for x in range(1, 9):
+for x in range(1, 10):
     print("------- [" + str(x) + "단] -------")
-    for y in range(1, 9):
+    for y in range(1, 10):
         print(x, "X", y, "=", x*y)
 print("---------------------")
